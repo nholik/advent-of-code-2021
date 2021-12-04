55175d19ff08f25547ccc3fa55d213644318002d
Nick Holik <nholik@gmail.com>
2021-12-03 20:16:20 -0500

    completed day 3

--- daythree/js/index.js
+++ daythree/js/index.js
@@ -3,23 +3,75 @@ import fs from "fs";
 const inputsReal = fs
   .readFileSync("./daythree/input.txt", "utf8")
   .split("\n")
-  .map((x) => x.split(''));
+  .map((x) => x.split(""));
+
+const testInputs = [
+  "00100",
+  "11110",
+  "10110",
+  "10111",
+  "10101",
+  "01111",
+  "00111",
+  "11100",
+  "10000",
+  "11001",
+  "00010",
+  "01010",
+].map((x) => x.split(""));
 
 const findGreeks = (inputs) => {
   const totalInputs = inputs.length;
-  const valsByPosition = inputs.reduce((acc, curr) => {
-    for (let i = 0; i < curr.length; i++) {
-      acc[i] += parseInt(curr[i]);
-    }
-    return acc;
-  }, new Array(inputs[0].length).fill(0));
-
-  const gammaInputs = valsByPosition.map((x) => (x > totalInputs / 2 ? "1" : "0"));
-  const epsilonInputs = gammaInputs.map((x) => (x === "1" ? "0" : "1"));
-  const gamma = parseInt(gammaInputs.join(""),2);
-  const epsilon = parseInt(epsilonInputs.join(""),2);
-
-  return gamma * epsilon;
+  return inputs
+    .reduce((acc, curr) => {
+      for (let i = 0; i < curr.length; i++) {
+        acc[i] += parseInt(curr[i]);
+      }
+      return acc;
+    }, new Array(inputs[0].length).fill(0))
+    .reduce(
+      (acc, curr) => {
+        return curr >= totalInputs / 2
+          ? [acc[0] + "1", acc[1] + "0"]
+          : [acc[0] + "0", acc[1] + "1"];
+      },
+      ["", ""]
+    );
 };
 
-export const dayThreePartA = () => findGreeks(inputsReal);
+const findRatings = (inputs) => {
+  const [ox, co] = inputs[0].reduce(
+    (candidates, _, index) => {
+      const [oxcandidate, cocandidates, currgamma, currepsilon] = candidates;
+      const gammaAtIndex = currgamma[index];
+      const epsilonAtIndex = currepsilon[index];
+
+      const newOxCandidate =
+        oxcandidate.length === 1
+          ? oxcandidate
+          : oxcandidate.filter((x) => x[index] === gammaAtIndex);
+
+      const newcocandidates =
+        cocandidates.length === 1
+          ? cocandidates
+          : cocandidates.filter((x) => x[index] === epsilonAtIndex);
+      const [nextgamma] = findGreeks(newOxCandidate);
+      const [__, nextepsilon] = findGreeks(newcocandidates);
+      return [newOxCandidate, newcocandidates, nextgamma, nextepsilon];
+    },
+    [inputs, inputs, ...findGreeks(inputs)]
+  );
+
+  const oxVal = ox[0].join("");
+  const coVal = co[0].join("");
+  return parseInt(oxVal, 2) * parseInt(coVal, 2);
+};
+
+export const dayThreePartA = () => {
+  const [g, e] = findGreeks(inputsReal);
+  return parseInt(g, 2) * parseInt(e, 2);
+};
+
+export const dayThreePartB = () => {
+  return findRatings(inputsReal);
+};
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import { dayOneA, dayOneB } from "./dayone/js/index.js";
 import { dayTwoPartA, dayTwoPartB } from "./daytwo/js/index.js";
-import { dayThreePartA } from "./daythree/js/index.js";
+import { dayThreePartA, dayThreePartB } from "./daythree/js/index.js";
 
-console.log(`Anser for day one part a: ${dayOneA()}`);
-console.log(`Anser for day one part b: ${dayOneB()}`);
+console.log(`Answer for day one part a: ${dayOneA()}`);
+console.log(`Answer for day one part b: ${dayOneB()}`);
 
-console.log(`Anser for two one part a: ${dayTwoPartA()}`);
-console.log(`Anser for two one part b: ${dayTwoPartB()}`);
+console.log(`Answer for two one part a: ${dayTwoPartA()}`);
+console.log(`Answer for two one part b: ${dayTwoPartB()}`);
 
-console.log(`Anser for three one part a: ${dayThreePartA()}`);
+console.log(`Answer for three one part a: ${dayThreePartA()}`);
+console.log(`Answer for three one part b: ${dayThreePartB()}`);
